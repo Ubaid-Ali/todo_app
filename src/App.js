f2e01c308e856fa6225a60e2db9f54be8ad01d7d
Ubaid Ali (locally) <syedubaidali2020@gmail.com>
2020-07-14 22:15:16 +0500

    Created a Todo App
    created a todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [transactions, setTransactions] = useState([])
+  let [name, setName] = useState("");
+
+  const inputChange = (e) => {
+    setName(name = e.target.value)
+    console.log(name)
+  }
+
+  const SubmitFnc = (e) => {
+    e.preventDefault()
+    setTransactions([...transactions, { name, id: Math.random() }])
+    setName(name = "")
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 > Todo App </h1>
+      <h4> Created by Ubaid Ali </h4>
+
+      <form onSubmit={SubmitFnc}>
+
+        <input onChange={inputChange}  value={name} placeholder="Enter Text..." required /> <br />
+        <button type="submit" onSubmit={SubmitFnc}
+          className="submit-btn"
+        >Add</button> <br />
+
+        <ul className='ul'>
+          {transactions.map((listItem) => {
+            return (
+              <li key={listItem.id}>
+                {listItem.name}
+
+                <button
+                  className="delete-btn"
+                  type="button"
+                  onClick={() => {
+                    setTransactions(transactions.filter((item) =>
+                      item.id !== listItem.id
+                    ))
+                  }} > x </button>
+
+              </li> )})}
+        </ul>
+      </form>
+
     </div>
   );
 }
